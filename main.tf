0351ddd7f194178bbda9ac729c339e83e4fb450b
H <hrvojep@yahoo.com>
2021-01-21 13:51:31 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,21 +10,26 @@ provider "random" {
 
 resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-  range_key      = "UserName"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-
-  attribute {
-    name = "UserName"
-    type = "S"
-  }
+resource "aws_instance" "basic" {
+  ami           = "ami-0ee1a20d6b0c6a347"
+  instance_type = "t3.nano"
 }
+
+# resource "aws_dynamodb_table" "tfc_example_table" {
+#   name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+#   read_capacity  = var.db_read_capacity
+#   write_capacity = var.db_write_capacity
+#   hash_key       = "UUID"
+#   range_key      = "UserName"
+
+#   attribute {
+#     name = "UUID"
+#     type = "S"
+#   }
+
+#   attribute {
+#     name = "UserName"
+#     type = "S"
+#   }
+# }
